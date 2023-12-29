cf73923f291a5ae014ff152c5bd99b119c336f9f
vuola <61627278+vuola@users.noreply.github.com>
2023-12-29 08:29:58 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Markus Vuolahti",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "markus.vuolahti@helsinki.fi",
+  gitHub: "University of Helsinki",
+  instagram: "University of Helsinki",
+  linkedIn: "markusvuolahti",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -37,22 +37,10 @@ const projectList = [
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Material Research project",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Course assignment for the course Material Research. The assignment was to make a simple atomistic simulator and use it to simulate the melting of a Lennard-Jones crystal.",
+    url: "https://github.com/vuola/mdsimu"
   },
 ];
 
